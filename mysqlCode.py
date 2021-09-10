6dd647e4cd3130d77b9587dc238c3daf6034a8da
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-10 15:33:44 -0400

    Added function stubs to mysqlCode.py

---
+++ mysqlCode.py
@@ -0,0 +1,18 @@
+
+
+#def getConnection(host, port, database, user, password):
+
+
+#def retrieveCustomerById(conn, customerNumber):
+
+
+#def retrieveCustomersByState(con,state):
+
+
+#def insertProductLine(conn, productLine,productDescription):
+
+
+#def updateProductLine(conn, productLine, htmlDescription):
+
+
+#def deleteProductLine(conn, productLine):

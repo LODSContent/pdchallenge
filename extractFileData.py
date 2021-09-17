3e2d99afd9f2a037cf7be16f398bb2406332b0b8
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-17 16:07:35 -0400

    updated extractFileData to work on Win & Linux

--- extractFileData.py
+++ extractFileData.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import sys
 import json
+import os
+
 
 from mysql.connector.abstracts import NAMED_TUPLE_CACHE
 
@@ -12,7 +14,9 @@ port = settings["sqlPort"]
 database = "classicmodels"
 user=settings["user"]
 password=settings["password"]
-filePath = "C:\\challenge-files-github\\python-data\\data\\{}.{}"
+directory_path = os.getcwd()
+
+filePath = "{}/{}/{}.{}"
 
 conn = mysql.connector.connect(user=user, password=password, host=host, port=port,database=database)
 sqlProducts = "select CONCAT('42{\"productCode\":\"', productCode, '\",\"productName\":\"', productName, '\",\"MSRP\": ', MSRP, '}') as product  from products;"
@@ -27,7 +31,7 @@ def saveCustomer():
     csr.execute(sqlCustomer)
     output = csr.fetchone()
     csr.close()
-    with open(filePath.format("customer","json"),"w") as f:
+    with open(filePath.format(directory_path,"data","customer","json"),"w") as f:
         json.dump(output,f,indent=2)
 
 
@@ -35,7 +39,7 @@ def saveCustomer():
 def saveProducts():
     csr = conn.cursor(named_tuple=True)
     csr.execute(sqlProducts)
-    with open(filePath.format("products","json"),"w") as f:
+    with open(filePath.format(directory_path,"data","products","json"),"w") as f:
         for product in csr:
             f.write("{}\n".format(product.product))
         csr.close()
@@ -53,7 +57,7 @@ def saveOrders():
         csrMonth = conn.cursor(named_tuple=True)
         csr = conn.cursor(named_tuple=True)
         csr.execute(sqlOrders,(month.orderYear, month.orderMonth))
-        with open(filePath.format("{}-{}".format(month.orderYear, month.orderMonth),"csv"),"w") as f:
+        with open(filePath.format(directory_path,"data","{}-{}".format(month.orderYear, month.orderMonth),"csv"),"w") as f:
             for order in csr:
                 f.write("{},{},{},{},{}\n".format(order.orderNumber, order.orderDate, order.status, order.customerNumber, order.orderTotal))
         csr.close()

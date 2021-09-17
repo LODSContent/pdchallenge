df1bcca0a17053cc5ac1d0e74b55e89528c454b7
Tracy Wallace <tracy.wallace@learnondemandsystems.com>
2021-09-17 15:49:33 -0400

    Added file data stuff

---
+++ fileCode.py
@@ -0,0 +1,16 @@
+
+
+
+def readCustomer(dataPath,customerNumber):
+    return 0
+
+
+
+def readProducts(dataPath,orderFilePrefix):
+    return 0
+
+
+
+def reportTopOrders(dataPath, transactionFileName):
+    return 0
+
